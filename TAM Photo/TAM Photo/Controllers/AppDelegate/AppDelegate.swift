935fd392975cb80da51366a907023a4fa318248e
Le Van Binh <binhlv@asiantech.vn>
2016-08-18 22:23:23 +0700

    [add] project initialization

---
+++ TAM Photo/TAM Photo/Controllers/AppDelegate/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  TAM Photo
+//
+//  Created by Le Van Binh on 8/18/16.
+//  Copyright © 2016 Le Van Binh. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.rootViewController = PhotoListViewController(nibName: "PhotoListViewController", bundle: nil)
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
