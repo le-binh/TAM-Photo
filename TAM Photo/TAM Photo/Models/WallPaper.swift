d049c5dd23c1b775c0a4157a913d28778389b972
Le Van Binh <binhlv@asiantech.vn>
2016-08-19 03:02:00 +0700

    [add] WallPaper model

---
+++ TAM Photo/TAM Photo/Models/WallPaper.swift
@@ -0,0 +1,29 @@
+//
+//  WallPaper.swift
+//  TAM Photo
+//
+//  Created by Le Van Binh on 8/19/16.
+//  Copyright © 2016 Le Van Binh. All rights reserved.
+//
+
+import UIKit
+
+class WallPaper: Mappable {
+    var title = ""
+    var imageURLString = ""
+    var thumbnailURLString = ""
+    var id: Double = 0
+    var timestamp = ""
+    
+    required init?(_ map: Map) {
+        
+    }
+    
+    func mapping(map: Map) {
+        title <- map["Title"]
+        imageURLString <- map["UrlImage"]
+        thumbnailURLString <- map["UrlThum"]
+        id <- map["PictureId"]
+        timestamp <- map["Timestamp"]
+    }
+}
